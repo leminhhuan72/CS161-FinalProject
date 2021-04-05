085a893d38ba9b18a52322430f789fbe6c187341
leminhhuan72 <leminhhuan72@gmail.com>
2021-04-05 21:08:07 +0700

    minh huan

--- Project1/Header.h
+++ Project1/Header.h
@@ -3,4 +3,23 @@
 #include <iostream>
 using namespace std; 
 
+struct schoolYr {
+
+};
+struct classR {
+
+};
+struct student {
+	int No;
+	char* Student ID; 
+	char* First name;
+	char *Last name: 
+	int Gender;
+	DOB Date_of_Birth;
+	char* Social ID;
+
+};
+struct DOB {
+	int date, month, year;
+};
 #endif 
