7ce9fbcb71126adfb50338843422424246dbbe71
viethoang1211 <81943892+viethoang1211@users.noreply.github.com>
2021-04-10 18:16:25 +0700

    course
    viet hoang

--- Project1/course.h
+++ Project1/course.h
@@ -1,13 +1,40 @@
 #ifndef _COURSE_H_
 #define _COURSE_H_
-
-
-
-
-struct course{
-
-//write something 
+struct date {
+	int d;
+	int m;
+	int y;
 };
+struct times {
+	int minu;
+	int hour;
+};
+struct session{
+	times time;
+	string day;
+};
+struct course{
+	int num; //so thu tu
+	string name;// ma mon hoc vi du CS162
+	string fullname;// ten mon hoc
+	times startTime;
+	times finishTime;
+	string classroom; //phong hoc
+	string clas; // lop hoc vi du 20ctt1, viet class khong duoc nen dung clas :(
+	date startDate;
+	date finishDate;
+	session first;
+	session second;
+	courseStruct* pNext;
+	infoLecture lecturer;
+};
+struct inforLecture {
+	string username;
+	string name;
+	string degree;
+	string gender;
+};
+
 
 
 
