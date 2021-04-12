b5097f8528f3b7827f57b8ed258ec968c84f48d7
viethoang1211 <81943892+viethoang1211@users.noreply.github.com>
2021-04-12 11:47:53 +0700

    fix

--- Project1/course.cpp
+++ Project1/course.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include<string.h>
+#define nullptr NULL
 #include "course.h"
 using namespace std;
 
-void courseRegis(course totalCourse) // 7
+void courseRegis(course*& totalCourse) // 7
 {
 	cout << "Please input the name of the new course(EX:CS162): ";
 	string name;
 	int count = 0;
 	cin.ignore();
-	cin.getline( name, '\n');
+	getline(cin, name, '\n');
 	//type of struct related to course 
 	course* cur = nullptr;
 	//a pointer points to list of course TotalCourse
@@ -18,7 +20,7 @@ void courseRegis(course totalCourse) // 7
 		while (cur != nullptr) {
 			if (cur->name == name) {
 				cout << "This course was avaliable!" << endl;
-				cout<<"enter any character to return back"
+				cout<<"enter any character to return back";
 				string u;
 				cin >> u;
 				return;
@@ -36,13 +38,13 @@ void courseRegis(course totalCourse) // 7
 		cur->num = count;
 		cur->name = name;
 		cout << "Full name: "<<endl;
-		cin.get(cur->fullname,'\n')
-		cout << "Classroom: "<,endl;
+		getline(cin,cur->fullname,'\n');
+		cout << "Classroom: "<<endl;
 		cin.ignore();
-		cin.get(cur->classroom,'\n')
+		getline(cin,cur->classroom,'\n');
 		cout<<"this course is intended for class :"<<endl;
 		cin.ignore();
-		cin.get(cur->clas,'\n')
+		getline(cin,cur->clas,'\n');
 		
 		cout << "Lecturer: " << endl;
 		cout << "  Username: "<<endl;
@@ -54,9 +56,9 @@ void courseRegis(course totalCourse) // 7
 		cout << "  Gender: "<<endl;
 		getline(cin, cur->lecturer.gender, '\n');
 		cout << "Started Date(date): "<<endl;
-		cin >> cur->startdate.d;
+		cin >> cur->startDate.d;
 		cout << "Started Date(month): "<<endl;
-		cin >> cur->startdate.m;
+		cin >> cur->startDate.m;
 		cout << "Started Date(year): "<<endl;
 		cin >> cur->startDate.y;
 		cout << "Finished Date(date): "<<endl;
@@ -66,94 +68,98 @@ void courseRegis(course totalCourse) // 7
 		cout << "Finished Date(year): "<<endl;
 		cin >> cur->finishDate.y;
 		cout << "Started Hour(hour): "<<endl;
-		cin >> cur->startTime.h;
+		cin >> cur->startTime.hour;
 		cout << "Started Hour(minute): "<<endl;
-		cin >> cur->startTime.m;
+		cin >> cur->startTime.minu;
 		cout << "Finished Hour(hour): "<<endl;
-		cin >> cur->finishTime.h;
+		cin >> cur->finishTime.hour;
 		cout << "Finished Hour(minute): "<<endl;
-		cin >> cur->finishTime.m;
+		cin >> cur->finishTime.minu;
 		cout<<"First session performed on: "<<endl;
 		cin.ignore();
-		cin.getline(cur->first.day,'\n');
-		cout<<"What time that session occurred: "<<endl
+		getline(cin,cur->first.day,'\n');
+		cout<<"What time that session occurred: "<<endl;
 		cout << "Started Hour(hour): "<<endl;
-		cin >> cur->first.day.time.h;
+		cin >> cur->first.time.hour;
 		cout << "Started Hour(minute): "<<endl;
-		cin >> cur->first.day.time.m;
+		cin >> cur->first.time.minu;
 		cout<<"Seconde session performed on: "<<endl;
 		cin.ignore();
-		cin.getline(cur->second.day,'\n');
-		cout<<"What time that session occurred: "<<endl
+		getline(cin,cur->second.day,'\n');
+		cout<<"What time that session occurred: "<<endl;
 		cout << "Started Hour(hour): "<<endl;
-		cin >> cur->second.day.time.h;
+		cin >> cur->second.time.hour;
 		cout << "Started Hour(minute): "<<endl;
-		cin >> cur->second.day.time.m;
+		cin >> cur->second.time.minu;
 		
 	}
 	else {
-		cur = new courseStruct;
+		cur = new course;
 		count = 1;
 		cur->num = count;
 		cur->name = name;
-		cout << "Full name: ";
-		
-	cin.get(cur->fullname,'\n')
-		cout << "Classroom: ";
+	cout << "Full name: "<<endl;
+		getline(cin,cur->fullname,'\n');
+		cout << "Classroom: "<<endl;
 		cin.ignore();
-		cin.get(cur->classroom,'\n')
-		cout<<"this course is intended for class :"
+		getline(cin,cur->classroom,'\n');
+		cout<<"this course is intended for class :"<<endl;
 		cin.ignore();
-		cin.get(cur->clas,'\n')
+		getline(cin,cur->clas,'\n');
 		
 		cout << "Lecturer: " << endl;
-		cout << "  Username: ";
+		cout << "  Username: "<<endl;
 		getline(cin, cur->lecturer.username, '\n');
-		cout << "  Name: ";
+		cout << "  Name: "<<endl;
 		getline(cin, cur->lecturer.name, '\n');
-		cout << "  Degree: ";
+		cout << "  Degree: "<<endl;
 		getline(cin, cur->lecturer.degree, '\n');
-		cout << "  Gender: ";
+		cout << "  Gender: "<<endl;
 		getline(cin, cur->lecturer.gender, '\n');
-		cout << "Started Date(date): ";
-		cin >> cur->startdate.d;
-		cout << "Started Date(month): ";
-		cin >> cur->startdate.m;
-		cout << "Started Date(year): ";
+		cout << "Started Date(date): "<<endl;
+		cin >> cur->startDate.d;
+		cout << "Started Date(month): "<<endl;
+		cin >> cur->startDate.m;
+		cout << "Started Date(year): "<<endl;
 		cin >> cur->startDate.y;
-		cout << "Finished Date(date): ";
+		cout << "Finished Date(date): "<<endl;
 		cin >> cur->finishDate.d;
-		cout << "Finished Date(month): ";
+		cout << "Finished Date(month): "<<endl;
 		cin >> cur->finishDate.m;
-		cout << "Finished Date(year): ";
+		cout << "Finished Date(year): "<<endl;
 		cin >> cur->finishDate.y;
-		cout << "Started Hour(hour): ";
-		cin >> cur->startTime.h;
-		cout << "Started Hour(minute): ";
-		cin >> cur->startTime.m;
-		cout << "Finished Hour(hour): ";
-		cin >> cur->finishTime.h;
-		cout << "Finished Hour(minute): ";
-		cin >> cur->finishTime.m;
+		cout << "Started Hour(hour): "<<endl;
+		cin >> cur->startTime.hour;
+		cout << "Started Hour(minute): "<<endl;
+		cin >> cur->startTime.minu;
+		cout << "Finished Hour(hour): "<<endl;
+		cin >> cur->finishTime.hour;
+		cout << "Finished Hour(minute): "<<endl;
+		cin >> cur->finishTime.minu;
 		cout<<"First session performed on: "<<endl;
 		cin.ignore();
-		cin.getline(cur->first.day,'\n');
-		cout<<"What time that session occurred: "<<endl
+		getline(cin,cur->first.day,'\n');
+		cout<<"What time that session occurred: "<<endl;
 		cout << "Started Hour(hour): "<<endl;
-		cin >> cur->first.day.time.h;
+		cin >> cur->first.time.hour;
 		cout << "Started Hour(minute): "<<endl;
-		cin >> cur->first.day.time.m;
+		cin >> cur->first.time.minu;
 		cout<<"Seconde session performed on: "<<endl;
 		cin.ignore();
-		cin.getline(cur->second.day,'\n');
-		cout<<"What time that session occurred: "<<endl
+		getline(cin,cur->second.day,'\n');
+		cout<<"What time that session occurred: "<<endl;
 		cout << "Started Hour(hour): "<<endl;
-		cin >> cur->second.day.time.h;
+		cin >> cur->second.time.hour;
 		cout << "Started Hour(minute): "<<endl;
-		cin >> cur->second.day.time.m;
+		cin >> cur->second.time.minu;
 		
-		pHeadCourse = cur;
+		totalCourse = cur;
 		cur = nullptr;
-		pHeadCourse->pNext = nullptr;
+		totalCourse->pNext = nullptr;
 	}
+}
+int main(){
+	course* test= new course;
+	courseRegis(test);
+	return 0;
 }
