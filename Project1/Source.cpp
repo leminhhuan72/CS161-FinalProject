8bfba04ba0bde517a8e7fe5c0fecbdcf024782fd
dinhmyky <79882659+dinhmyky@users.noreply.github.com>
2021-04-12 13:05:22 +0700

    fix student function call in source.cpp
    Fix path in function call

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -19,15 +19,15 @@ int main()
 	ifstream fin;
 	ofstream fout;
 	classR apcs1, apcs2, clc1, clc2, vp;
-	createAClass("20APCS1", apcs1, fin);
-	addStudent2Class("20APCS1.txt", apcs2, fin);
+	createAClass("20APCS1.txt", apcs1, fin);
+	addStudent2Class("20APCS1.txt", apcs1, fin);
 	createAClass("20APCS2.txt", apcs2, fin);
 	addStudent2Class("20APCS2.txt", apcs2, fin);
-	createAClass("20CLC1", clc1, fin);
-	addStudent2Class("20CLC1.txt", apcs2, fin);
-	createAClass("20CLC2", clc1, fin);
-	addStudent2Class("20CLC2.txt", apcs2, fin);
-	createAClass("20VP", vp, fin);
-	addStudent2Class("20VP.txt", apcs2, fin);
+	createAClass("20CLC1.txt", clc1, fin);
+	addStudent2Class("20CLC1.txt", clc1, fin);
+	createAClass("20CLC2.txt", clc2, fin);
+	addStudent2Class("20CLC2.txt", clc1, fin);
+	createAClass("20VP.txt", vp, fin);
+	addStudent2Class("20VP.txt", clc1, fin);
 	return 0;
 }
